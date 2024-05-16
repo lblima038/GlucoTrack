77156263ce9e2b0d02ce51d3d509fed2777c3abe
Lucas <163474080+lblima038@users.noreply.github.com>
2024-05-16 00:52:45 -0300

    Add files via upload

---
+++ registro_gligecima.py
@@ -0,0 +1,21 @@
+from dao.glicemiaDAO import GlicemiaDAO
+from datetime import datetime
+
+def cadastrar_glicemia(codigo_paciente):
+
+    data = input("Informe a data do registro de glicose (dd/mm/aaaa): ")
+    conv = datetime.strptime(data, '%d/%m/%Y')
+    dia = conv.day
+    mes = conv.month
+    ano = conv.year
+    print("Data registrada: ", conv.day, "/", conv.month, "/", conv.year)
+
+    valor = int(input("Indique o valor de glicose registrada: "))
+
+    # implemetar o código que extrai da variavel 'data' o valor do dia, do mes e do ano
+    #
+    #
+    
+    glicemiaDAO = GlicemiaDAO()
+    glicemiaDAO.inserir(codigo_paciente, dia, mes, ano, valor)
+
