959055da064b7fd024192e9e3fc3329f957d1670
Joao Rietra <jhlrietra@gmail.com>
2024-05-17 17:41:05 -0300

    medicacao

--- entidades/glicemia.py
+++ entidades/glicemia.py
@@ -3,6 +3,7 @@ class Glicemia:
     def __init__(self, codigo, codigo_paciente, dia, mes, ano, valor):
         self.codigo = codigo
         self.codigo_paciente = codigo_paciente
+        # TODO trocar esses 3 ints por 1 datetime
         self.dia = dia
         self.mes = mes
         self.ano = ano
