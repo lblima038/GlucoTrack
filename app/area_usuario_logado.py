5d2234014a9b47b63cb35ef844edf084189da851
Joao Rietra <jhlrietra@gmail.com>
2024-06-05 01:23:33 -0300

    Edit

--- app/area_usuario_logado.py
+++ app/area_usuario_logado.py
@@ -1,6 +1,9 @@
 from util.util import *
 from app.cadastro_paciente import *
 from app.cadastro_medicacao import *
+from app.cadastro_glicemia import *
+from app.cadastro_usuario import *
+from app.cadastro_alimento import *
 from crud.crud_paciente import *
 
 def tela_principal(codigo_usuario):
